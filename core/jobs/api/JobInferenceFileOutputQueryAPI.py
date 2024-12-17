7d2a4b5a42f080fb1e9c896ef07ee79a5a07c808
yenon118 <yenon118@gmail.com>
2024-12-17 10:30:29 -0600

    Added job inference file output query APIs

---
+++ core/jobs/api/JobInferenceFileOutputQueryAPI.py
@@ -0,0 +1,53 @@
+from django.db.models import Q
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import authentication_classes, permission_classes
+
+from ..models.JobInferenceFileOutputModel import JobInferenceFileOutputModel
+
+from ..serializers.JobInferenceFileOutputModelSerializer import JobInferenceFileOutputModelSerializer
+
+
+@api_view(['GET'])
+def JobInferenceFileOutputQuery(request):
+    if request.method == 'GET':
+        try:
+            job_inference_file_creation_user = request.user.id
+
+            job_inference_file_output = JobInferenceFileOutputModel.objects.filter(
+                job_inference_file_creation_user=job_inference_file_creation_user
+            )
+
+            response_object = {
+                "isJobInferenceFileOutputQuery": True,
+                "JobInferenceFileOutput": JobInferenceFileOutputModelSerializer(job_inference_file_output, many=True).data
+            }
+            return Response(response_object)
+        except Exception as e:
+            return Response({"isJobInferenceFileOutputQuery": False, "error": str(e)}, status=500)
+
+    return Response({"isJobInferenceFileOutputQuery": False, "error": "Invalid request method"}, status=405)
+
+
+@api_view(['GET'])
+def JobInferenceFileOutputQueryByID(request):
+    if request.method == 'GET':
+        try:
+            job_inference_file_output_id = request.data['job_inference_file_output_id']
+            job_inference_file_creation_user = request.user.id
+
+            job_inference_file_output = JobInferenceFileOutputModel.objects.get(
+                pk=job_inference_file_output_id,
+                job_inference_file_creation_user=job_inference_file_creation_user
+            )
+
+            response_object = {
+                "isJobInferenceFileOutputQueryByID": True,
+                "JobInferenceFileOutput": JobInferenceFileOutputModelSerializer(job_inference_file_output).data
+            }
+            return Response(response_object)
+        except Exception as e:
+            return Response({"isJobInferenceFileOutputQueryByID": False, "error": str(e)}, status=500)
+
+    return Response({"isJobInferenceFileOutputQueryByID": False, "error": "Invalid request method"}, status=405)
--- core/jobs/urls.py
+++ core/jobs/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path
 from .api.TestAPI import Test
 from .api.JobInferenceAPI import JobInference
 from .api.JobInferenceQueryAPI import JobInferenceQuery
+from .api.JobInferenceFileOutputQueryAPI import JobInferenceFileOutputQuery, JobInferenceFileOutputQueryByID
 
 
 app_name = 'jobs'
@@ -12,4 +13,6 @@ urlpatterns = [
     re_path('api/job_test/', Test, name='api_job_test'),
     re_path('api/job_inference/', JobInference, name='api_job_inference'),
     re_path('api/job_inference_query/', JobInferenceQuery, name='api_job_inference_query'),
+    re_path('api/job_inference_file_output_query/', JobInferenceFileOutputQuery, name='api_job_inference_file_output_query'),
+    re_path('api/job_inference_file_output_query_by_id/', JobInferenceFileOutputQueryByID, name='api_job_inference_file_output_query_by_id'),
 ]
